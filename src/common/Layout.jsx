c34cb8fe71225ca82d597e8c41882b04d55d9eb7
mirella2-3 <zmeo89@gmail.com>
2025-11-01 21:50:36 +0900

    헤더추가

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Footer from './Footer';
+import Header from './Header';
 
 const Layout = () => {
     const [showHeader, setShowHeader] = useState(true);
